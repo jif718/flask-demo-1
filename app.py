89a3693843f17cdff024057c58defb21ea52401e
YIFAN JIA <jif@outlook.com>
2026-05-08 10:36:52 +0800

    update monitoring

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from metrics import init_metrics
+
 app = Flask(__name__)
+init_metrics(app)
 
-@app.route('/')
+
+@app.route("/")
 def hello():
-    return "Hello from Flask Demo v1.8\n"
+    return "Hello from Flask Demo v1.9\n"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+
+@app.route("/health")
+def health():
+    return "ok\n"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
